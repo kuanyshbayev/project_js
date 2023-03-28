b429a9ec268514c57c7843666f3cc5105d94ffaf
Nurbol <kuanysbayev.nurbol@gmail.com>
2023-03-28 15:35:02 +0500

    first

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = prompt("вам есть 18?", "18");
+// console.log(typeof(result));
+
+const answers = [];
+
+answers[0] = prompt('kak vawe imya?', '');
+answers[1] = prompt('kak vawe fam?', '');
+answers[2] = prompt('kak vawe otec?', '');
+
+document.write(answers);
